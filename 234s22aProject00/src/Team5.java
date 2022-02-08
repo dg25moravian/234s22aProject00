9048db8823f9735858e2f45174a057651d98ca28
Davin Glynn <glynnd@moravian.edu>
2022-02-08 09:39:43 -0500

    Davin Glynn

---
+++ 234s22aProject00/src/Team5.java
@@ -0,0 +1,18 @@
+
+
+public class Team5 extends Team
+{
+    public Team5(String name)
+    {
+        super(name);
+        createTeamMembers();
+    }
+
+    public void createTeamMembers()
+    {
+        members.add(new TeamMember("Davin Glynn"));
+        // create a new member class for each team member
+        // and add it to the team members list
+    }
+}
+
