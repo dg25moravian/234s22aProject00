d3bb90cb8269a430288418fbbed240334bb2fb59
Kyle <adamsk04@moravian.edu>
2022-02-08 09:44:52 -0500

    Added Kyle Adams

--- 234s22aProject00/src/Team5.java
+++ 234s22aProject00/src/Team5.java
@@ -11,7 +11,6 @@ public class Team5 extends Team
     public void createTeamMembers()
     {
         members.add(new TeamMember("Davin Glynn"));
-
         members.add(new TeamMember("Kyle Adams"));
         // create a new member class for each team member
         // and add it to the team members list
